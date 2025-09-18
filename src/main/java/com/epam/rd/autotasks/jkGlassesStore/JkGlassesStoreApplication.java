b13617719b95b15817392b8c00afd3d81e7703be
Jokola Kistauri <jkist19@freeuni.edu.ge>
2025-09-18 21:48:47 +0400

    test

---
+++ src/main/java/com/epam/rd/autotasks/jkGlassesStore/JkGlassesStoreApplication.java
@@ -0,0 +1,13 @@
+package com.epam.rd.autotasks.jkGlassesStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JkGlassesStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JkGlassesStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/epam/rd/autotasks/jkGlassesStore/JkGlassesStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.rd.autotasks.jkGlassesStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JkGlassesStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
